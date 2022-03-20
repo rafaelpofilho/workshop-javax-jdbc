599ec791a570946f026347d3e900cca352ecdde4
Rafael Filho <soft_rafael@hotmail.com>
2022-03-20 10:46:33 -0300

    About View

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,11 +1,20 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable{
 
@@ -30,10 +39,37 @@ public class MainViewController implements Initializable{
 	
 	@FXML
 	public void onMnuItemAboutAction() {
-		System.out.println("onMnuItemAboutAction");
+		loadView("/gui/About.fxml");
 	}
 	
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
+	}
+	
+	private synchronized void loadView(String absoluteName) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+			
+			Scene mainScene = Main.getMainScene();
+			
+			// pega o 1o elemento da tela principal (ScrollPane)
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			
+			// Guardar o mainMenu para adicionar depois (1o filho de VBox)
+			Node mainMenu = mainVBox.getChildren().get(0);
+					
+			// Apagar os filhos atuais do VBox para depois adicionar novamente junto com os novos ítens da tela que será apresentada
+			mainVBox.getChildren().clear();
+			
+			// Adicionar novamente o mainMenu do formulário principal
+			mainVBox.getChildren().add(mainMenu);
+			
+			// Adicionar os ítens da tela que será chamada
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+		}
+		catch(IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 }
